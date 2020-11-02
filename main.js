9b4ca6e3e36288045fef62fa009044d9f5c9dbd9
EranHolzman <eranhol1993@gmail.com>
2020-11-02 10:38:07 +0200

    DOM

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// console.log(document)
+// DOM
+// Exercise 1
+
+
+const rightArrow = document.getElementById("right")
+rightArrow.onclick = function (){
+  let block = document.getElementById("block")
+  let right = parseInt(block.style.left) || 0
+  right += 15
+  block.style.left = right + "px"
+}
+
+
+
+
+
